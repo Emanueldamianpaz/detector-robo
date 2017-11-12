5560d46934067caa938665edf47e76ea21bdc907
Emanuel <emanueldamian.paz@gmail.com>
2017-11-12 14:34:22 -0300

    Revisar el previewCamera

--- app/src/main/java/com/davinci/edu/detector_robo/MainActivity.java
+++ app/src/main/java/com/davinci/edu/detector_robo/MainActivity.java
@@ -1,18 +1,27 @@
 package com.davinci.edu.detector_robo;
 
-import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.SurfaceTexture;
+import android.hardware.Camera;
+import android.hardware.Camera.CameraInfo;
+
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.davinci.edu.detector_robo.cameraController.PhotoHandler;
 import com.davinci.edu.detector_robo.pedometer.StepDetector;
 import com.davinci.edu.detector_robo.pedometer.StepListener;
+
+import java.io.IOException;
 
 
 public class MainActivity extends AppCompatActivity implements SensorEventListener, StepListener {
@@ -24,6 +33,12 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
     private int numSteps;
     private TextView TvSteps;
     private Button BtnStart, BtnStop;
+
+    private static Camera camera = null;
+
+
+    private int cameraId = 0;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -86,10 +101,61 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         TvSteps.setText(TEXT_NUM_STEPS + numSteps);
 
 
-        /**
-         * TODO Acá hay que hacer un metodo que saque fotos y envie email
-         * */
+        if (!getPackageManager().hasSystemFeature(PackageManager.FEATURE_CAMERA)) {
+            Toast.makeText(this, "No camera on this device", Toast.LENGTH_LONG).show();
+        } else {
+            cameraId = findFrontFacingCamera();
+            if (cameraId < 0) {
+                Toast.makeText(this, "No front facing camera found.", Toast.LENGTH_LONG).show();
+            } else {
+                try {
+
+                    camera = Camera.open(cameraId);
+                } catch (Exception e) {
+                    Log.e(getString(R.string.app_name), "failed to open Camera");
+                    e.printStackTrace();
+                }
+            }
+        }
+
+        SurfaceTexture st = new SurfaceTexture(MODE_PRIVATE);
+        try {
+            camera.setPreviewTexture(st);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+/* TODO Falla el preview*/
+        camera.startPreview();
+        camera.takePicture(null, null, new PhotoHandler(getBaseContext()));
+
     }
+
+    @Override
+    protected void onPause() {
+        if (camera != null) {
+            camera.release();
+            camera = null;
+        }
+        super.onPause();
+    }
+
+
+    private int findFrontFacingCamera() {
+        int cameraId = -1;
+        // Search for the front facing camera
+        int numberOfCameras = Camera.getNumberOfCameras();
+        for (int i = 0; i < numberOfCameras; i++) {
+            CameraInfo info = new CameraInfo();
+            Camera.getCameraInfo(i, info);
+            if (info.facing == CameraInfo.CAMERA_FACING_BACK) {
+                Log.d("Controller", "Camera found");
+                cameraId = i;
+                break;
+            }
+        }
+        return cameraId;
+    }
+
 
 }
 
---
+++ app/src/main/java/com/davinci/edu/detector_robo/cameraController/PhotoHandler.java
@@ -0,0 +1,70 @@
+package com.davinci.edu.detector_robo.cameraController;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import android.content.Context;
+import android.content.Intent;
+import android.hardware.Camera;
+import android.hardware.Camera.PictureCallback;
+import android.net.Uri;
+import android.os.Environment;
+import android.util.Log;
+
+public class PhotoHandler implements PictureCallback {
+
+    private Context context;
+
+    public PhotoHandler(Context context) {
+        this.context = context;
+    }
+
+    @Override
+    public void onPictureTaken(byte[] data, Camera camera) {
+
+        File pictureFileDir = getDir();
+
+        if (!pictureFileDir.exists() && !pictureFileDir.mkdirs()) {
+
+            Log.d("Camara", "Can't create directory to save image.");
+            return;
+
+        }
+
+        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyymmddhhmmss");
+        String date = dateFormat.format(new Date());
+        String photoFile = "Picture_" + date + ".jpg";
+
+        String filename = pictureFileDir.getPath() + File.separator + photoFile;
+
+        File pictureFile = new File(filename);
+
+        try {
+            FileOutputStream fos = new FileOutputStream(pictureFile);
+            fos.write(data);
+            fos.close();
+
+
+            Intent i = new Intent(Intent.ACTION_SEND);
+            i.putExtra(Intent.EXTRA_EMAIL, new String[]{"emanueldamian.paz@gmail.com"});
+            i.putExtra(Intent.EXTRA_SUBJECT, "Aplicación de prueba");
+            i.putExtra(Intent.EXTRA_STREAM, Uri.fromFile(pictureFile));
+
+            i.setType("image/png");
+            context.startActivity(Intent.createChooser(i, "Share you on the jobing"));
+
+
+            Log.d("Camara", "New Image saved:" + photoFile);
+        } catch (Exception error) {
+            Log.d("Camara", "File" + filename + "not saved: " + error.getMessage());
+        }
+    }
+
+    private File getDir() {
+        File sdDir = Environment
+                .getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES);
+        return new File(sdDir, "CameraAPIDemo");
+    }
+}
